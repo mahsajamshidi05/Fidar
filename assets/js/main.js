3d96429a6d03dcda0c502550f7bfdcef6f10cea1
mahsajamshidi05 <fatemejamshidi8@gmail.com>
2023-02-10 19:45:17 +0330

    added js

---
+++ assets/js/main.js
@@ -0,0 +1,265 @@
+﻿(function($){
+	"use strict";
+	jQuery(document).on('ready', function () {
+
+		// Mean Menu
+		jQuery('.mean-menu').meanmenu({
+			meanScreenWidth: "991"
+        });
+
+        // Header Sticky
+		$(window).on('scroll',function() {
+            if ($(this).scrollTop() > 120){
+                $('.navbar-area').addClass("is-sticky");
+            }
+            else{
+                $('.navbar-area').removeClass("is-sticky");
+            }
+        });
+
+        // Search Popup JS
+        $('.close-btn').on('click',function() {
+            $('.search-overlay').fadeOut();
+            $('.search-btn').show();
+            $('.close-btn').removeClass('active');
+        });
+        $('.search-btn').on('click',function() {
+            $(this).hide();
+            $('.search-overlay').fadeIn();
+            $('.close-btn').addClass('active');
+        });
+
+        // Popup Video
+		$('.popup-youtube').magnificPopup({
+			disableOn: 320,
+			type: 'iframe',
+			mainClass: 'mfp-fade',
+			removalDelay: 160,
+			preloader: false,
+			fixedContentPos: false
+        });
+
+        // Input Plus & Minus Number JS
+        $('.input-counter').each(function() {
+            var spinner = jQuery(this),
+            input = spinner.find('input[type="text"]'),
+            btnUp = spinner.find('.plus-btn'),
+            btnDown = spinner.find('.minus-btn'),
+            min = input.attr('min'),
+            max = input.attr('max');
+            
+            btnUp.on('click', function() {
+                var oldValue = parseFloat(input.val());
+                if (oldValue >= max) {
+                    var newVal = oldValue;
+                } else {
+                    var newVal = oldValue + 1;
+                }
+                spinner.find("input").val(newVal);
+                spinner.find("input").trigger("change");
+            });
+            btnDown.on('click', function() {
+                var oldValue = parseFloat(input.val());
+                if (oldValue <= min) {
+                    var newVal = oldValue;
+                } else {
+                    var newVal = oldValue - 1;
+                }
+                spinner.find("input").val(newVal);
+                spinner.find("input").trigger("change");
+            });
+        });
+		
+		// Feedback Slides
+		$('.feedback-slides').owlCarousel({
+			loop: true,
+			nav: true,
+            dots: true,
+            rtl: true,
+			autoHeight: true,
+			autoplayHoverPause: true,
+            autoplay: true,
+            mouseDrag: true,
+			animateOut: 'fadeOut',
+			items: 1,
+            navText: [
+                "<i class='flaticon-left-chevron'></i>",
+                "<i class='flaticon-right-chevron'></i>"
+            ],
+        });
+
+        // Tabs
+        (function ($) {
+            $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+            $('.tab ul.tabs li a').on('click', function (g) {
+                var tab = $(this).closest('.tab'), 
+                index = $(this).closest('li').index();
+                tab.find('ul.tabs > li').removeClass('current');
+                $(this).closest('li').addClass('current');
+                tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
+                tab.find('.tab_content').find('div.tabs_item:eq(' + index + ')').slideDown();
+                g.preventDefault();
+            });
+        })(jQuery);
+        
+        // Nice Select JS
+        $('select').niceSelect();
+		
+		// Tabs
+        (function ($) {
+            $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+            $('.tab ul.tabs li a').on('click', function (g) {
+                var tab = $(this).closest('.tab'), 
+                index = $(this).closest('li').index();
+                tab.find('ul.tabs > li').removeClass('current');
+                $(this).closest('li').addClass('current');
+                tab.find('.tab-content').find('div.tabs-item').not('div.tabs-item:eq(' + index + ')').slideUp();
+                tab.find('.tab-content').find('div.tabs-item:eq(' + index + ')').slideDown();
+                g.preventDefault();
+            });
+        })(jQuery);
+        
+        // Count Time 
+        function makeTimer() {
+            var endTime = new Date("September 30, 2020 17:00:00 PDT");			
+            var endTime = (Date.parse(endTime)) / 1000;
+            var now = new Date();
+            var now = (Date.parse(now) / 1000);
+            var timeLeft = endTime - now;
+            var days = Math.floor(timeLeft / 86400); 
+            var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+            var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+            var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+            if (hours < "10") { hours = "0" + hours; }
+            if (minutes < "10") { minutes = "0" + minutes; }
+            if (seconds < "10") { seconds = "0" + seconds; }
+            $("#days").html(days + "<span>روز</span>");
+            $("#hours").html(hours + "<span>ساعت</span>");
+            $("#minutes").html(minutes + "<span>دقیقه</span>");
+            $("#seconds").html(seconds + "<span>ثانیه</span>");
+        }
+        setInterval(function() { makeTimer(); }, 1000);
+
+        // FAQ Accordion
+        $(function() {
+            $('.accordion').find('.accordion-title').on('click', function(){
+                // Adds Active Class
+                $(this).toggleClass('active');
+                // Expand or Collapse This Panel
+                $(this).next().slideToggle('fast');
+                // Hide The Other Panels
+                $('.accordion-content').not($(this).next()).slideUp('fast');
+                // Removes Active Class From Other Titles
+                $('.accordion-title').not($(this)).removeClass('active');		
+            });
+        });
+		
+		// Go to Top
+        $(function(){
+            // Scroll Event
+            $(window).on('scroll', function(){
+                var scrolled = $(window).scrollTop();
+                if (scrolled > 300) $('.go-top').addClass('active');
+                if (scrolled < 300) $('.go-top').removeClass('active');
+            });  
+            // Click Event
+            $('.go-top').on('click', function() {
+                $("html, body").animate({ scrollTop: "0" },  500);
+            });
+        });
+        
+    });
+    
+    // Preloader Area
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	});
+}(jQuery));
+// saq
+let faqLabel=document.querySelectorAll(".faq-label");
+
+faqLabel.forEach(item=>item.onclick=()=>{
+   //selektuje Answer
+   item.nextElementSibling.classList.toggle('active');
+   
+   
+   let labelIcon=item.lastElementChild;
+   let icons=labelIcon.lastElementChild;
+   icons.classList.toggle('rotate');
+ 
+   
+})
+
+// counter
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 1500,
+        easing: 'linear',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+// Partner Slides
+$('.partner-slides').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    rtl: true,
+    autoplayHoverPause: true,
+    autoplay: true,
+    mouseDrag: true,
+    margin: 30,
+    navText: [
+        "<i class='flaticon-arrow-pointing-to-left'></i>",
+        "<i class='flaticon-arrow-pointing-to-right'></i>"
+    ],
+    responsive: {
+        0: {
+            items: 2,
+        },
+        576: {
+            items: 3,
+        },
+        768: {
+            items: 4,
+        },
+        1200: {
+            items: 6,
+        },
+    }
+});
+// Projects Slides
+$('.projects-slides').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    rtl: true,
+    autoplayHoverPause: true,
+    autoplay: true,
+    mouseDrag: false,
+    navText: [
+        "<i class='flaticon-arrow-pointing-to-right'></i>",
+        "<i class='flaticon-arrow-pointing-to-left'></i>"
+    ],
+    responsive: {
+        0: {
+            items: 1,
+        },
+        576: {
+            items: 2,
+        },
+        768: {
+            items: 2,
+        },
+        1200: {
+            items: 3,
+        },
+        1400: {
+            items: 4,
+        }
+    }
+});
